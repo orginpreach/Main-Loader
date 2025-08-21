8e2d125a1220f89da84aab6a3d1cf3a12caed8dd
orginpreach <mcramjeff@gmail.com>
2025-08-21 17:27:12 +0800

    Create Zenon.cc

---
+++ Zenon.cc
@@ -0,0 +1,11 @@
+-- I love my gf so much <3
+
+local GameId = game.PlaceId
+
+if GameId == 12625784503 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/orginpreach/Saber-Showdown/refs/heads/main/You%20can't%20skid%20this%20gng"))()
+end
+
+if GameId == 130650085400942 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/orginpreach/Aladia-PvP/refs/heads/main/You%20can't%20skid%20this%20gng"))()
+end
