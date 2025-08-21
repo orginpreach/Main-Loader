f020136cb1db332a2c8e856aa269aaf14d313f8d
orginpreach <mcramjeff@gmail.com>
2025-08-21 23:53:24 +0800

    Update Zenon.cc

--- Zenon.cc
+++ Zenon.cc
@@ -1,11 +1,15 @@
 -- I love my gf so much <3
 
-local GameId = game.PlaceId
+local GameId = game.GameId
 
-if GameId == 12625784503 then
+if GameId == 4430449940 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/orginpreach/Saber-Showdown/refs/heads/main/You%20can't%20skid%20this%20gng"))()
 end
 
-if GameId == 130650085400942 then
+if GameId == 8050914790 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/orginpreach/Aladia-PvP/refs/heads/main/You%20can't%20skid%20this%20gng"))()
+end
+
+if GameId == 4931927012 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/orginpreach/Basketball-Legends/refs/heads/main/You%20can't%20skid%20this%20gng"))()
 end
